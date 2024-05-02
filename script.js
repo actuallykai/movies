b1f74f70d4df03f99fa09b775e9c7f522dae509a
actuallykai <actuallykai@outlook.com>
2024-05-02 16:45:32 -0400

    added fetching movies

---
+++ script.js
@@ -0,0 +1,46 @@
+var haveFetchedMovies = false;
+
+var movieDataContainer = document.querySelector(".movie-data-container");
+
+function onMoviesRequested() {
+    if (haveFetchedMovies) {
+        return;
+    }
+    fetchMovies();
+    haveFetchedMovies = true;
+}
+
+function generateMovieCard(title, director, producer, releaseDate) {
+    var movieCard = document.createElement("div");
+    movieCard.classList.add("movie-card");
+
+    var titleText = document.createElement("h2");
+    titleText.classList.add("movie-title");
+    titleText.innerHTML = title;
+
+    var creditsText = document.createElement("p");
+    creditsText.classList.add("movie-credits");
+    creditsText.innerHTML = "Directed by <b>" + director + "</b><br>Produced by <b>" + producer + "</b><br>Released <b>" + releaseDate + "</b>";
+
+    movieCard.appendChild(titleText);
+    movieCard.appendChild(creditsText);
+
+    movieDataContainer.appendChild(movieCard);
+}
+
+async function fetchMovies() {
+    const response = await fetch("https://swapi.dev/api/films?format=json");
+    const moviesData = await response.json();
+
+    const moviesDataStripped = moviesData.results;
+    
+    for (let i = 0; i < moviesData.count; i++) {
+        const currentMovieData = moviesDataStripped[i];
+        generateMovieCard(
+            currentMovieData.title, 
+            currentMovieData.director, 
+            currentMovieData.producer, 
+            currentMovieData.release_date,
+        )
+    }
+}
